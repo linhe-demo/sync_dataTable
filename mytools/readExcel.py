be012b5740a7da307dddfa6defee1a42e9fbe9a6
muhe <muhe@i9i8.com>
2022-08-17 14:14:37 +0800

    优化

---
+++ mytools/readExcel.py
@@ -0,0 +1,13 @@
+from tools.readFile import read_excl
+
+if __name__ == "__main__":
+    data = read_excl("../data/demo.xlsx")
+    feature1 = data[:, 0:1]  # 取第一列
+    feature2 = data[:, 1:2]  # 取第二列
+    lists = []
+    m = 0
+    for i in feature1:
+        tmpKey = str(feature1[m][0]) + feature2[m][0]
+        lists.append(tmpKey)
+        m += 1
+    print("'" + "','".join(lists) + "'")
--- tools/readFile.py
+++ tools/readFile.py
@@ -1,7 +1,15 @@
 import json
 
+import pandas as pd
+
 
 def read_json_config():
     with open(r"D:\PythonProject\sync_dataTable\config\config.json") as json_file:
         config = json.load(json_file)
     return config
+
+
+def read_excl(filePath):
+    file_path = "r'" + filePath + "'"  # r对路径进行转义，windows需要
+    raw_data = pd.read_excel(filePath, header=0)  # header=0表示第一行是表头，就自动去除了
+    return raw_data.values
