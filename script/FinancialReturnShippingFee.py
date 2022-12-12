4e99a062df7850d272512a967c773d02e6631d9d
muhe <muhe@i9i8.com>
2022-12-12 10:39:36 +0800

    财务分析-退款时效分析

--- script/FinancialReturnShippingFee.py
+++ script/FinancialReturnShippingFee.py
@@ -1,5 +1,6 @@
 #! /usr/bin/env python3
 # 财务分析-匹配对应的退货物流收入
+# 【数据拉取】财务分析-退款时效分析（oak-4331）
 from datetime import timedelta, datetime
 
 from tools.dbLink import getAll
@@ -54,6 +55,35 @@ def financialReturnShippingFee(bDate, eDate, fileName, filePath):
     sendEmail("数据报表", "退货物流收入", ["tansuan@kerrylan.com"], fileName, filePath, False)
 
 
+def financialRefundTimeFee(bDate, eDate, fileName, filePath):
+    printLog("退货申请时间物流数据拉取", None)
+    exportData = []
+    # 获取满足条件的发货订单信息
+    try:
+        sql = sqlmap('getRefundDateInfo')
+        results = getAll(sql, ('%Y-%m-%d', '%Y-%m', bDate, eDate))
+        for row in results:
+            exportData.append({
+                '订单号': row['taobao_order_sn'],
+                '退货申请时间': row['r_date'],
+                '发货月份': row['s_date'],
+                '退款完成时间': row['finishDate'],
+                '退款状态': row['r_status']
+            })
+    except Exception as e:
+        raise e
+
+    # 写入excel
+    saveToExcel({0: exportData},
+                {0: "明细"},
+                {0: ['订单号', '退货申请时间', '发货月份', '退款完成时间', '退款状态']},
+                filePath)
+    sendEmail("数据报表", "退货物流收入", ["tansuan@kerrylan.com"], fileName, filePath, False)
+
+
 if __name__ == "__main__":
-    financialReturnShippingFee("2022-08-01", "2022-10-01", 'financialReturnShippingFee.xlsx',
-                               '../data/financialReturnShippingFee.xlsx')
+    # financialReturnShippingFee("2022-08-01", "2022-10-01", 'financialReturnShippingFee.xlsx',
+    #                            '../data/financialReturnShippingFee.xlsx')
+
+    financialRefundTimeFee("2022-08-01 00:00:00", "2022-11-30 23:59:59", 'financialRefundTimeFee.xlsx',
+                           '../data/financialRefundTimeFee.xlsx')
--- sqlmap/finanicalSql.py
+++ sqlmap/finanicalSql.py
@@ -29,6 +29,33 @@ def sqlmap(index):
 	                shipping_time >= UNIX_TIMESTAMP('%s')
 	        AND 
 	                shipping_time < UNIX_TIMESTAMP('%s')
-        '''
+        ''',
+        "getRefundDateInfo": '''
+            SELECT
+                    eoi.taobao_order_sn,
+                    FROM_UNIXTIME( UNIX_TIMESTAMP( ra.create_time ), '%s' ) AS r_date,
+                    FROM_UNIXTIME( UNIX_TIMESTAMP( r.CHECK_DATE_3 ), '%s' ) AS s_date,
+                    r.CHECK_DATE_3 AS finishDate,
+                    CASE
+                        r.`STATUS` 
+                        WHEN 'RFND_STTS_INIT' THEN
+                        '新建' 
+                        WHEN 'RFND_STTS_IN_CHECK' THEN
+                        '审核' 
+                        WHEN 'RFND_STTS_EXECUTED' THEN
+                        '完成' 
+                        WHEN 'RFND_STTS_CHECK_OK' THEN
+                        '待退款' 
+                    END AS r_status 
+                FROM
+                    romeo.refund r
+                    INNER JOIN ecshop.return_apply ra ON ra.return_apply_id = r.return_apply_id
+                    INNER JOIN ecshop.ecs_order_info eoi ON eoi.order_id = r.ORDER_ID 
+                WHERE
+                    ra.create_time >= '%s' 
+                    AND ra.create_time <= '%s' 
+                    AND r.REFUND_TYPE_ID = 5 
+                    AND r.`STATUS` IN ('RFND_STTS_EXECUTED', 'RFND_STTS_INIT', 'RFND_STTS_IN_CHECK', 'RFND_STTS_CHECK_OK')
+                        '''
     }
     return sql[index]
