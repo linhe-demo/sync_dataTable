ec57b923533929d7c8f38f420ab32acfae9cca83
muhe <muhe@i9i8.com>
2023-01-29 09:45:28 +0800

    更新

--- script/FinancialReturnShippingFee.py
+++ script/FinancialReturnShippingFee.py
@@ -81,12 +81,12 @@ def financialRefundTimeFee(bDate, eDate, fileName, filePath):
                 {0: "明细"},
                 {0: ['订单号', '退货申请时间', '发货月份', '退款完成时间', '品类ID', '退款状态', '国家']},
                 filePath)
-    # sendEmail("数据报表", "退货物流收入", ["tansuan@kerrylan.com"], fileName, filePath, True)
+    sendEmail("数据报表", "退货退款时效分析", ["tansuan@kerrylan.com"], fileName, filePath, True)
 
 
 if __name__ == "__main__":
-    financialReturnShippingFee("2022-01-01", "2022-08-31", 'financialReturnShippingFee.xlsx',
-                               '../data/financialReturnShippingFee.xlsx')
+    financialReturnShippingFee("2022-09-12", "2022-12-31", 'financialReturnShippingFee.xlsx',
+                               '../data/financialReturnShippingFee.xlsx')  # 退货物流收入
 
-    # financialRefundTimeFee("2022-09-01 00:00:00", "2022-10-31 23:59:59", 'financialRefundTimeFee.xlsx',
-    #                        '../data/financialRefundTimeFee.xlsx')
+    financialRefundTimeFee("2022-10-03 00:00:00", "2023-01-29 23:59:59", 'financialRefundTimeFee.xlsx',
+                           '../data/financialRefundTimeFee.xlsx')  # 退货时效分析
