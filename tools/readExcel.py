f4f97e8fd89438ba287b88f5c7466f45d74354e5
muhe <muhe@i9i8.com>
2023-02-28 17:49:48 +0800

    更新

--- tools/readExcel.py
+++ tools/readExcel.py
@@ -5,6 +5,7 @@ import pandas as pd
 from tools.readFile import read_excl
 
 
+# 读取excel中指定列整列元素，返回一个集合
 def readExcelData(filePath, column):
     df = pd.read_excel(filePath, usecols=[column - 1])  # 指定读取的列
     df_list = df.values.tolist()
@@ -12,7 +13,6 @@ def readExcelData(filePath, column):
     for i in df_list:
         backList.append(i[0])
     return backList
-
 
 # def readExcelData(filePath, column):
 #     data = read_excl(filePath)  # 文件位置
