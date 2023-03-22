332ac5e2f9daa2b19a2a3bc1fb19357c0ac03597
muhe <muhe@i9i8.com>
2023-03-22 09:32:45 +0800

    新增 令牌桶限流策略

--- pkg/tokenbucket/bucket.py
+++ pkg/tokenbucket/bucket.py
@@ -2,13 +2,18 @@ import time
 
 
 class TokenBucket(object):
-
+    """
+        rate 每秒新增令牌数
+        capacity 桶容量
+    """
     def __init__(self, rate, capacity):
         self.rate = rate
         self.capacity = capacity
         self.currentAmount = 0
         self.lastConsumeTime = int(time.time())
-
+    """
+        tokenAmount 需要的令牌数
+    """
     def consume(self, tokenAmount):
         timeDiff = (int(time.time()) - self.lastConsumeTime) * self.rate
         self.currentAmount = min(timeDiff + self.currentAmount, self.capacity)
