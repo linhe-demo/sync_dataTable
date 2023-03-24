3a1e054e213b0923d89b853f1ee74c6150cb0b91
muhe <muhe@i9i8.com>
2023-03-24 15:29:53 +0800

    新增 pdf 下载功能

---
+++ tools/download.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+import os
+import requests
+
+
+def download_pdf_file(url: str, filepath) -> bool:
+    """Download PDF from given URL to local directory.
+
+    :param url: The url of the PDF file to be downloaded
+    :return: True if PDF file was successfully downloaded, otherwise False.
+    """
+
+    # Request URL and get response object
+    response = requests.get(url, stream=True)
+
+    # isolate PDF filename from URL
+    pdf_file_name = os.path.basename(url)
+    if response.status_code == 200:
+        # Save in current working directory
+        # filepath = os.path.join(os.getcwd(), pdf_file_name)
+        with open(filepath, 'wb') as pdf_object:
+            pdf_object.write(response.content)
+            print(f'{pdf_file_name} was successfully saved!')
+            return True
+    else:
+        print(f'Uh oh! Could not download {pdf_file_name},')
+        print(f'HTTP response status code: {response.status_code}')
+        return False
